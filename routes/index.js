6fbe57ef6acf047aacb31fedbe444114c1e90d8b
Ceydi Y. Cuenca <ceycuenca20@gmail.com>
2023-04-28 18:24:40 +0200

    Primer push

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*1. Crea una aplicación Express que tenga una única ruta que devuelva un
+mensaje de "Hola mundo".*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hola mundo, Ceydi Cuenca' });
+});
+
+/* 2. Amplía la aplicación anterior para que tenga dos rutas: una que devuelva un
+mensaje de "Hola" y otra que devuelva un mensaje de "Mundo" */
+router.get('/hola', function(req, res, next) {
+  res.render('index', { title: 'Holitas' });
+});
+router.get('/mundo', function(req, res, next) {
+  res.render('index', { title: 'mundo' });
 });
 
 module.exports = router;
